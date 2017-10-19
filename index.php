09006b37fa5ac1335d667e2e8e07a4e36203280e
Zeyel <tim.branstett@gmail.com>
2017-10-19 11:24:09 +0200

    +index.php / +style.css / -Testus.txt

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Cocktails</title>
+</head>
+
+<header>include "header.php"</header>
+
+<body></body>
+
+<footer>include "footer.php"</footer>
+</html>
