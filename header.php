f1db1f321aaec5053be1bbef90cb6ca8488f4375
NwotYknuf <jlafille@ensc.fr>
2017-11-11 18:59:38 +0100

    index css

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <div class="container-fluid">
 	<!-- Bar de connexion-->
 	<div class ="row">
+		<div class='col-md-4 text-center'>
+			<h1>Cocktailotron</h1>
+		</div>
 		<div class ="col float-right">
 			<div class="float-right">
 				<form class="form-inline">
@@ -10,13 +13,6 @@
 				</form>
 				<a href="index.php?page=inscription">Pas encore inscrit ?</a>
 			</div>
-		</div>
-	</div>
-	
-	<!-- Bandeau -->
-	<div class="row">
-		<div class ="col">
-			<h1>Cocktailotron</h1>
 		</div>
 	</div>
 </div>
